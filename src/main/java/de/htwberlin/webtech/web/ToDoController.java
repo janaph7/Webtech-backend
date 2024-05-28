ed7247c538a4afe2080431a1de1726667aacdc64
Jana Pham <phamjana7@gmail.com>
2024-05-28 18:50:47 +0200

    1

--- src/main/java/de/htwberlin/webtech/web/ToDoController.java
+++ src/main/java/de/htwberlin/webtech/web/ToDoController.java
@@ -1,5 +1,6 @@
 package de.htwberlin.webtech.web;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin (origins ={"http://localhost:5173/"})
 @RestController
 @RequestMapping("/ToDos")
 public class ToDoController {
