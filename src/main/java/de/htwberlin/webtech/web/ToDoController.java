2eecfb330849b1aa83c08a6d67e2ffb56e85c094
Jana Pham <phamjana7@gmail.com>
2024-06-30 18:25:26 +0200

    neu controller und service

--- src/main/java/de/htwberlin/webtech/web/ToDoController.java
+++ src/main/java/de/htwberlin/webtech/web/ToDoController.java
@@ -4,37 +4,38 @@ import de.htwberlin.webtech.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
-@RestController
+
+@RestController()
 public class ToDoController {
 
     @Autowired
     ToDoService toDoService;
 
-    @GetMapping
-    @RequestMapping("/getTodos")
+    @CrossOrigin
+    @GetMapping("/getToDos")
     public Iterable<ToDo> getToDos() {
         return toDoService.getalltodos();
     }
 
-    @PostMapping
-    @RequestMapping("/ToDos")
+    @CrossOrigin
+    @PostMapping("/ToDos")
     public ToDo createToDo(@RequestBody ToDo toDo) {
         return toDoService.save(toDo);
     }
 
-    @DeleteMapping
-    @RequestMapping("deleteToDos/{id}")
+    @CrossOrigin
+    @DeleteMapping("deleteToDos/{id}")
     public void deleteToDo(@PathVariable String id) {
         Long entryId = Long.parseLong(id);
         toDoService.delete(entryId);
     }
 
-    @PostMapping
-    @RequestMapping("toggleCompleted/{id}")
+    @CrossOrigin
+    @PostMapping("toggleCompleted/{id}")
     public ToDo toggleCompleted(@RequestBody ToDo toDo, @PathVariable String id) {
         Long entryId = Long.parseLong(id);
         toDoService.delete(entryId);
         return toDoService.save(toDo);
     }
+
 }
