d5c143cc110324acd6db5f0f3bb75e9c933f03de
elisakhoury <elisakh@icloud.com>
2024-06-30 16:54:45 +0200

    Datenbanktreiber eintragen für H2

--- src/test/java/de/htwberlin/webtech/WebtechbackendApplicationTests.java
+++ src/test/java/de/htwberlin/webtech/WebtechbackendApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @Disabled
 @SpringBootTest
-class WebtechbackendApplicationTests {
+class  WebtechbackendApplicationTests {
 
 	@Test
 	void contextLoads() {
